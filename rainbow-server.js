b65423125f753cf9a0e4eaaf93f08d11e37e7569
shinekaze <shinekaze@gmail.com>
2020-03-20 18:14:08 +0100

    Rearranged the Routing Behavior, added sites for an index and for a 404 error handling. Diversified the methods for the user to enter an address to be sent to the correct page.

--- rainbow-server.js
+++ rainbow-server.js
@@ -1,42 +1,27 @@
-var url = require('url');
+//var url = require('url');
 var fs = require('fs');
 var http = require('http'); // Import Node.js core module
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 
 var server = http.createServer(function (req, res) {   //create web server
-    if (req.url == '/') { //check the URL of the current request
-
-// set response header
-        res.writeHead(200, {'Content-Type': 'text/html'});
-
-// set response content
-        res.write('<html><body><p>This is home Page.</p></body></html>');
-        res.end();
-
-    } else if (req.url == "/patient.html") {
-
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        var q = url.parse(req.url, true);
-        var filename = "./rainbow-patient/web" + q.pathname;
-        fs.readFile(filename, function (err, data) {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end("404 Not Found");
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    } else if (req.url == "/student.html") {
-
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
-        res.end();
-
-    } else
-        res.end('Invalid Request!');
-
+    console.log('Request received: ' + req.url);
+    if(req.url === ' '||req.url === '/index'||req.url === '/home'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/Index.html').pipe(res);
+    }
+    else if(req.url === '/patient' || req.url === '/patients'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/rainbow-patient/web/patient.html').pipe(res);
+    }
+    else if(req.url === '/Personal'|| req.url === '/personnel'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/Personal.html').pipe(res);
+    }
+    else{
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
 });
 
 
