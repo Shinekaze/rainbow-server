1a3b3cc8c68a4610044a7c1f88bef8a432026601
shinekaze <shinekaze@gmail.com>
2020-03-21 10:41:28 +0100

    Basic DB push (write) functionality added to newpatient.ejs and mongoDB. See ReqList for output.

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -1,19 +1,60 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/*Post functionality for adding to database*/
+router.post('/addpatient', function(req, res){
+  var db = req.db;
+
+  //capture request variables for handoff to database
+  var BedNumber = req.body.BedNumber;
+  var Help = req.body.Help;
+  var Prio = req.body.Prio;
+
+  var collection = db.get('usercollect');
+
+  //insert new information to usercollect database
+  collection.insert({
+    "BedNumber": BedNumber,
+    "Help": Help,
+    "Prio": Prio,
+    "TimeIn": Date()
+  }, function (err, doc){
+    if(err) {
+      //error handling
+      res.send("Error in adding record to database");
+    }
+    else{
+      //forward to patient page
+      res.redirect("patient");
+    }
+  });
+});
+
+
+/* GET home page - two possibilities for entry. */
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
-router.get('/index', function(req, res, next) {
+router.get('/index', function(req, res) {
   res.render('index', { title: 'Express' });
 });
-router.get('/patient', function(req, res, next){
+
+// direct to patient page
+router.get('/patient', function(req, res){
   res.render('patient', {title: 'Express'});
 });
-router.get('/personal', function(req, res, next){
+
+//direct to nurse page
+router.get('/personal', function(req, res){
   res.render('personal', {title: 'Express'});
 });
+
+//direct to page for adding new patient entries -------- to be modified/reused later
+router.get('/newpatient', function(req, res){
+  res.render('newpatient', {title: 'Express'});
+});
+
+//direct to page for basic output of database info ------ to be modified/reused later
 router.get('/reqlist', function(req, res){
   var db = req.db;
   var collection = db.get('usercollect');
